e52628154ca0346c18ad4d191a80793e60438be0
J. Tabares <jatabares78@gmail.com>
2026-06-30 22:52:33 -0500

    fix: import PrimeReactProvider from @primereact/core

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { PrimeReactProvider } from 'primereact/api'
+import { PrimeReactProvider } from '@primereact/core'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
